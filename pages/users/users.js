aadbb672fa8c0cb1786fe13b7888806fda6e957b
iris <429328398@qq.com>
2020-06-11 11:31:07 +0800

    修改user

--- pages/users/users.js
+++ pages/users/users.js
@@ -1,59 +1,237 @@
 //index.js
 //获取应用实例
-const app = getApp()
 
+// pages/mine/mine.js
+const app = getApp()
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: '欢迎',
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isHide: false,
+
+    continuousDay:0,//连续打卡天数
+    totalDay:0, //总打卡天数
+    totalItem:0,//总记账笔数
+
+    firstTime:'0',
+    flag:true
   },
+
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function() {
+    var that = this;
+    // 查看是否授权
+    wx.getSetting({
+      success: function(res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function(res) {
+              app.globalData.userInfo = res.userInfo
+              that.setData({
+                userInfo: res.userInfo,
+              })
+              // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
+              // 根据自己的需求有其他操作再补充
+              // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
+              wx.login({
+                success: res => {
+                  // 获取到用户的 code 之后：res.code
+                  // console.log("用户的code:" + res.code);
+                  // 可以传给后台，再经过解析获取用户的 openid
+                  // 或者可以直接使用微信的提供的接口直接获取 openid ，方法如下：
+                  // wx.request({
+                  //     // 自行补上自己的 APPID 和 SECRET
+                  //     url: 'https://api.weixin.qq.com/sns/jscode2session?appid=自己的APPID&secret=自己的SECRET&js_code=' + res.code + '&grant_type=authorization_code',
+                  //     success: res => {
+                  //         // 获取到用户的 openid
+                  //         console.log("用户的openid:" + res.data.openid);
+                  //     }
+                  // });
+                }
+              });
+            }
+          });
+        } else {
+          // 用户没有授权
+          // 改变 isHide 的值，显示授权页面
+          that.setData({
+            isHide: true
+          });
         }
+      }
+    });
+  },
+
+  bindGetUserInfo: function(e) {
+    if (e.detail.userInfo) {
+      //用户按了允许授权按钮
+      var that = this;
+      // 获取到用户的信息了，打印到控制台上看下
+      console.log("用户的信息如下：");
+      console.log(e.detail.userInfo);
+      //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+      that.setData({
+        isHide: false,
+        userInfo: e.detail.userInfo
+      });
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        showCancel: false,
+        confirmText: '返回授权',
+        success: function(res) {
+          // 用户没有授权成功，不需要改变 isHide 的值
+          if (res.confirm) {
+            console.log('用户点击了“返回授权”');
+          }
+        }
+      });
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+  },
+
+  /**
+   * 用户点击"打卡”按钮
+   */
+  // switch:function(e){
+  //   // const length = this.data.dateArray.length
+  //   // needToAdd = false //判断是否连续打卡
+  //   // if(length == 0){
+  //   //   date = (new Date()).toString()
+  //   //   this.data.dateArray = [date].concat(this.data.dateArray)
+  //   //   this.setData({
+  //   //     dateArray : this.data.dateArray
+  //   //   })
+  //   // }
+  //   // date = new Date()
+  //   // this.data.dateArray = this.data.dateArray.concat(date)
+  //   this.data.continuousDay = this.data.continuousDay + 1
+  //   this.setData({
+  //     continuousDay: this.data.continuousDay,
+  //     //dateArray: this.data.dateArray
+  //   })
+  //   wx.showToast({
+  //     title: "打卡成功",
+  //     duration:2000,
+  //     icon:"success",
+  //   })
+  // }
+  switch:function(){
+    var D=(new Date()).getDate().toString();
+    if(D != wx.getStorageSync('D')){
+      wx.setStorageSync('D', D);
+      wx.setStorage({
+        key: 'FirstTime',
+        data: (parseInt(this.data.firstTime) + 1).toString(),
+      })
+      var that = this;
+      var firstTime = wx.getStorage({
+        key: 'FirstTime',
+        success: function (res) {
+          that.setData({
+            firstTime: res.data,
+            flag:false
+          })
+          wx.showToast({
+            title: '打卡成功！',
+            icon: 'success',
+            duration: 1200,
+            mask: true
+          })
+        },
+      })
+    }else{
+      wx.showToast({
+        title: '今日打卡已完成！',
+        icon:'loading',
+        duration:1200,
+        mask:true
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  onShow:function(options){
+    var that = this;
+    var firstTime = wx.getStorage({
+      key: 'FirstTime',
+      success: function (res) {
+        that.setData({
+          firstTime: res.data
+        })
+      },
     })
+    var D = (new Date()).getDate().toString();
+    if (D != wx.getStorageSync('D')){
+      this.setData({
+        flag:true
+      })
+    }else{
+      this.setData({
+        flag:false
+      })
+    }
   },
-  jumpToIndex: function(){
-    wx.navigateTo({
-      url: '../index/index',
-    })
-  }
 })
+
